4cd5a00ec792aaf375a01efe9929427d98aef303
local <local@i5.com>
2023-10-17 19:04:36 +0800

    php1.0

--- index.php
+++ index.php
@@ -1,10 +1,15 @@
+<?php
+$data = require './data/data.php';
+$num = 10;
+?>
+
 <!DOCTYPE html>
 <html lang="zh-CN">
 
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>欢迎来到信条</title>
+    <title>信条，一个分享和交流的平台</title>
     <link rel="stylesheet" href="./css/index.css" />
     <link rel="stylesheet" href="./css/nav.css" />
     <link rel="stylesheet" href="./css/content.css" />
@@ -12,51 +17,35 @@
 </head>
 
 <body class="index">
-
     <?php require './view/nav.html' ?><!-- 导航栏 -->
-
     <div class="content"><!-- 内容区域 -->
-
         <div class="article"> <!-- 文章区域 -->
 
-            <div class="box">
-                <a href="#"></a>
-                <p></p>
-            </div>
-            <div class="box">
-                <a href="#"></a>
-            </div>
-            <div class="box">
-                <a href="#"></a>
-            </div>
-            <div class="box">
-                <a href="#"></a>
-            </div>
+
+
             <div class="box"></div>
-            <div class="box"></div>
-            <div class="box"></div>
-            <div class="box"></div>
+            <div class="box">
+                <div><a href="#"><?php echo $data['title']; ?></a></div>
+                <div><span><?php echo $data['text']; ?></span></div>
+            </div>
+            <?php
+            for ($i = 0; $i < $num; ++$i) {
+                echo '<div class="box"></div>';
+            };
+            ?>
+
+
+
 
             <hr>
-
             <?php require './view/footer.html' ?><!-- 底部区域 -->
-
         </div>
-
         <div class="sidebar"><!-- 侧边栏区域 -->
-
-            <div class="hot-question"><!-- 其他区域 -->
-
-            </div>
-
+            <div class="hot-question"><!-- 其他区域 --></div>
             <hr>
-
             <?php require './view/footer.html' ?><!-- 底部区域 -->
-
         </div>
-
     </div>
-
 </body>
 
 </html>
