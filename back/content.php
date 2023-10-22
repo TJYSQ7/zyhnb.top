f6fe5d9ddd3cf8f8c3abca6ce2cd65b19411aa16
local <local@i5.com>
2023-10-22 10:27:47 +0800

    v4.0开发版-新增删除

--- back/content.php
+++ back/content.php
@@ -12,16 +12,19 @@
 <body class="b">
     <?php require './nav.html' ?>
     <div class="content"><!-- 内容区域 -->
+        <div class="head">标题</div>
 
         <?php require '../common/conn.php' ?><!-- 连接数据库 -->
         <?php require '../data/content_list.php' ?>
         <?php while ($row = mysqli_fetch_array($select_1)) { ?>
 
-
-            <h3><?= $row['title'] ?></h3>
-            <hr>
+            <div class="list"><?= $row['title'] ?></div>
 
         <?php }; ?>
+        <form action="../data/content_del.php" method="post">
+            <input type="text" name="title" placeholder="输入要删除的标题">
+            <input type="submit" value="删除">
+        </form>
         <?php require '../common/break.php' ?><!-- 断开连接 -->
 
 
---
+++ data/content_del.php
@@ -0,0 +1,12 @@
+<?php
+$title = $_POST['title'];
+require '../common/conn.php';
+$utf8 = mysqli_query($conn, "set names utf8mb4");
+$sql = "delete from content_list where title='$title';";
+$del = mysqli_query($conn, $sql);
+if ($del) {
+    echo '删除成功';
+} else {
+    echo '删除失败';
+}
+require '../common/break.php';
