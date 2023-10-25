b4ff2e0dfdff41a802e08d7708eec6af4b49fb21
local <local@i5.com>
2023-10-26 00:24:56 +0800

    用户页面

--- php/user.php
+++ php/user.php
@@ -13,10 +13,31 @@
 <body>
     <?php require '../view/nav.php' ?>
     <main>
-        <div class="top"></div>
+        <div class="top">
+            <div class="background"></div>
+            <div class="main">
+                <img src="../images/default.png" alt="">
+                <div class="info">
+                    <h2>amazing</h2>
+                    <h3>♂️</h3>
+                </div>
+            </div>
+        </div>
         <div class="bottom">
-            <div class="left"></div>
-            <div class="right"></div>
+            <div class="left">
+                <ul>
+                    <li><a href="">文章</a></li>
+                    <li><a href="">文章</a></li>
+                </ul>
+            </div>
+            <div class="right">
+                <div class="create">
+                    <img src="../images/create.png" alt="">
+                    <span>创作中心</span>
+                </div>
+                <div></div>
+                <div></div>
+            </div>
         </div>
     </main>
 </body>
